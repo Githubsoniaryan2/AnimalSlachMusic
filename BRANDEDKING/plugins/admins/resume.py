af3823728796814410ed532fba0e2bfb1ea4a5f0
SoniAryanXmart <154493242+Githubsoniaryan@users.noreply.github.com>
2024-01-26 13:42:19 +0530

    𓆩ᴀɴɪᴍᴀʟ𓆪™〆ᴍᴜsɪᴄ𓃮

--- BRANDEDKING/plugins/admins/resume.py
+++ BRANDEDKING/plugins/admins/resume.py
@@ -19,3 +19,5 @@ async def resume_com(cli, message: Message, _, chat_id):
     await message.reply_text(
         _["admin_4"].format(message.from_user.mention), reply_markup=close_markup(_)
     )
+
+
